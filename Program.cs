de4800fa3e46932aca50ce92c46a2c2a1ac76ea3
Matthew Morrow <99447524+MatthewMorrow@users.noreply.github.com>
2025-03-09 20:18:57 +0100

    Add C969

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+using System.Windows.Forms;
+using SchedulingSystem.Data;
+using SchedulingSystem.Forms;
+
+namespace SchedulingSystem
+{
+    public static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            try
+            {
+                var connectionString = ConfigurationManager
+                    .ConnectionStrings["client_schedule"]
+                    .ConnectionString;
+                var db = new DatabaseAccess(connectionString);
+                Application.Run(new LoginForm(db));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Critical Application Error: " + ex.Message,
+                    "Application Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+        }
+    }
+}
